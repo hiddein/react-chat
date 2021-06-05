f7ebc7c4555b12874e9d0ac23c2776f7c8854f9f
hiddein <nikolaev.devl@gmail.com>
2021-06-05 18:53:36 +0300

    final commit

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,6 @@
-import io from "socket.io-client"
-const socket = io("http://localhost:3500/")
+import io from 'socket.io-client';
 
-export default socket
+const socket = io('http://localhost:3500/');
+
+export default socket;
+
--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 const express = require("express")
-
-
+const cors = require("cors")
 const app = express()
 const server = require("http").Server(app)
-const io = require("socket.io")(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  })
+const io = require("socket.io")(server, { cors: { origin: "*" } })
 
 const rooms = new Map()
+app.use(cors())
+app.use(express.json())
 
-app.get("/rooms", (req, res) => {
-    res.json(rooms)
-})
+app.get('/rooms/:id', (req, res) => {
+  const { id: roomId } = req.params;
+  const obj = rooms.has(roomId)
+    ? {
+        users: [...rooms.get(roomId).get('users').values()],
+        messages: [...rooms.get(roomId).get('messages').values()],
+      }
+    : { users: [], messages: [] };
+  res.json(obj);
+});
 
-io.on('connection', socket => {
+app.post('/rooms', (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ['users', new Map()],
+        ['messages', []],
+      ]),
+    );
+  }
+  res.send();
+});
 
-    console.log('user connected', socket.id);
+io.on('connection', (socket) => {
+  socket.on('ROOM:JOIN', ({ roomId, userName }) => {
+    socket.join(roomId);
+    rooms.get(roomId).get('users').set(socket.id, userName);
+    const users = [...rooms.get(roomId).get('users').values()];
+    socket.to(roomId).emit('ROOM:SET_USERS', users);
+  });
 
-})
+  socket.on('ROOM:NEW_MESSAGE', ({ roomId, userName, text }) => {
+    const obj = {
+      userName,
+      text,
+    };
+    rooms.get(roomId).get('messages').push(obj);
+    socket.to(roomId).emit('ROOM:NEW_MESSAGE', obj);
+  });
+
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()];
+        socket.to(roomId).emit('ROOM:SET_USERS', users);
+      }
+    });
+  });
+
+  console.log('user connected', socket.id);
+});
 
 server.listen(3500, (err) => {
   if (err) {
-    throw new Error(err)
+    throw Error(err);
   }
-  console.log("Server started")
-})
+  console.log('Сервер запущен!');
+});
